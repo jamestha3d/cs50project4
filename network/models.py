1d45db8c02af0cdf912d82f5dc91a95a7992a9e2
Jamie <jamestha3d@Macintoshs-MacBook-Pro.local>
2022-03-11 17:50:38 +0100

    old version

--- network/models.py
+++ network/models.py
@@ -2,5 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractUser, models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    about = models.TextField(blank=True, null=True)
+    #image = models.ImageField(default="", upload_to="profile_pics")
+
+
+class post(models.Model):
+    #models
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    content = models.TextField(blank=False)
+    date = models.DateTimeField(default=datetime.now())
+    liked = models.ManyToManyField('User', default=None, blank=True, related_name='post_likes')
+
+    def __str__(self):
+        return f" Post {self.id} made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
+
